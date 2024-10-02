d82bcc1731afad7c378abf7ed7471cc7268d5e87
MagicKrazik <datacon.org@gmail.com>
2024-10-01 18:26:57 -0600

    Update settings.py
    secure settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vq8i24=a=^n_-d#78kmx@w68px1nlhbi&=qe1l!ox0(al8_4tn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -165,11 +165,11 @@ CACHES = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'carlos.horacio.olivares@gmail.com'
-EMAIL_HOST_PASSWORD = 'zhtu fjgf kbyw klat'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_FROM_EMAIL = 'noreply@torresamaurel.com'
 
